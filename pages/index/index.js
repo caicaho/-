e49a5250580b33f17443f2dbbfed49158c2ba15f
caichao <caichao@nicetuan.net>
2020-12-05 00:49:59 +0800

    tabbar

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '首页',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
